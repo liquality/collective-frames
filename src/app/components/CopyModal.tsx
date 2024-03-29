b13a2b0d5f981e0fe884f29230bbb646e4dc8d97
Johanna Fransson <adrina0002@gmail.com>
2024-03-29 12:40:52 +0100

    fix close btn modal position

--- src/app/components/CopyModal.tsx
+++ src/app/components/CopyModal.tsx
@@ -22,7 +22,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
 
   return (
     <div className="fixed top-0 left-0 flex justify-center items-center w-full h-full bg-black bg-opacity-50 z-50">
-      <div className="bg-white border border-black rounded-lg p-8">
+      <div className="relative bg-white border border-black rounded-lg p-8">
         <button
           className="absolute top-2 right-2 text-black hover:text-gray-500 focus:outline-none"
           onClick={onClose}
