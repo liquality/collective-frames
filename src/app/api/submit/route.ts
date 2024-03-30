9627dca542e025cb16adc8279b654b1abd16e9f1
Johanna Fransson <adrina0002@gmail.com>
2024-03-30 10:54:18 +0100

    add zora sdk stuff and pinata api key

--- src/app/api/submit/route.ts
+++ src/app/api/submit/route.ts
@@ -1,5 +1,39 @@
-export async function GET() {
-    console.log('Dataaa')
+import type { PublicClient, WalletClient } from "viem";
+import { create1155CreatorClient } from "@zoralabs/protocol-sdk";
+
+
+export async function POST(req: Request) {
+    const body = await req.json();
+    console.log(body, 'wat body?')
+
+
 
     return Response.json({ data: "HI" })
+}
+
+export async function create1155Contract({
+    publicClient,
+    walletClient,
+    creator,
+}: {
+    publicClient: PublicClient;
+    walletClient: WalletClient;
+    creator: `0x${string}`
+}) {
+    const demoTokenMetadataURI = "ipfs://DUMMY/token.json";
+    const demoContractMetadataURI = "ipfs://DUMMY/contract.json";
+    const creatorClient = create1155CreatorClient({ publicClient });
+    const { request } = await creatorClient.createNew1155Token({
+        contract: {
+            name: "testContract",
+            uri: demoContractMetadataURI,
+        },
+        tokenMetadataURI: demoTokenMetadataURI,
+        account: creator,
+        mintToCreatorCount: 1,
+    });
+    const { request: simulateRequest } = await publicClient.simulateContract(request);
+    const hash = await walletClient.writeContract(simulateRequest);
+    const receipt = await publicClient.waitForTransactionReceipt({ hash });
+    return receipt;
 }
