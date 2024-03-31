ad6550b8e939321d929dd1de0f4950406fe605f8
Bradley Suira <bradleysuira@gmail.com>
2024-03-31 16:14:23 -0500

    feat: first draft for user creation post handler

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ import { frame, collective, user } from "@/db";
 
 export type NewFrame = typeof frame.$inferInsert;
 export type CollectiveItem = typeof collective.$inferSelect;
+export type NewUser = typeof user.$inferInsert;
 
 export type CreateFrameResult = NewFrame | {
     imageUrl: string;
