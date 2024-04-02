d4017ac671a0799df6e1cca87f54bf3ee69422ef
Bradley Suira <bradleysuira@gmail.com>
2024-04-02 09:40:25 -0500

    feat: updated ui

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,8 @@
-// Navbar.tsx
 "use client";
 import React, { useEffect } from "react";
 import { useApp } from "@/context/AppContext";
 import useLocalStorage from "@/hooks/use-local-storage-state";
+import NeynarButton from "./NeynarButton";
 
 const Navbar = () => {
   const { setSignerUuid, setFid } = useApp();
@@ -67,16 +67,8 @@ const Navbar = () => {
       <div className="flex items-center space-x-4 font-mono text-lg lg:flex">
         Liquality Nft Meme Generator
       </div>
-      <div
-        className="neynar_signin flex"
-        data-client_id="YOUR_NEYNAR_CLIENT_ID"
-        data-success-callback="onSignInSuccess"
-        data-theme="dark"
-      ></div>
-      <script
-        src="https://neynarxyz.github.io/siwn/raw/1.2.0/index.js"
-        async
-      ></script>
+      <NeynarButton />
+     
       <script></script>
     </nav>
   );
