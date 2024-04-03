25a8455135c15314cb2c7984397bca00cff71929
Johanna Fransson <adrina0002@gmail.com>
2024-04-03 14:12:40 +0200

    now casting on users behalf works

--- src/utils/neynar.ts
+++ src/utils/neynar.ts
@@ -6,11 +6,11 @@ import { toast } from "react-toastify";
 
 
 //This file handles all read and write using Neynar Client sdk
-const client = new NeynarAPIClient("NEYNAR_API_DOCS" || "");
+const client = new NeynarAPIClient(process.env.NEXT_PUBLIC_NEYNAR_API_KEY || "");
 
 export const writeCast = (text: string) => {
     console.log(Auth.getUser.signerUuid, 'wats signer?')
     if (text) {
-        client.publishCast(Auth.getUser.signerUuid, text,)
+        client.publishCast(Auth.getUser.signerUuid, text)
     } else toast("Cast needs to minimum 1 charachter")
 };
