b55d83349ee7320478178dadc0db6e680430d5ca
Johanna Fransson <adrina0002@gmail.com>
2024-04-03 14:17:46 +0200

    remove else statement because text length is handled in html

--- src/utils/neynar.ts
+++ src/utils/neynar.ts
@@ -1,7 +1,6 @@
 
 import { NeynarAPIClient } from "@neynar/nodejs-sdk";
 import { Auth } from "./cookie-auth";
-import { toast } from "react-toastify";
 
 
 
@@ -10,7 +9,5 @@ const client = new NeynarAPIClient(process.env.NEXT_PUBLIC_NEYNAR_API_KEY || "")
 
 export const writeCast = (text: string) => {
     console.log(Auth.getUser.signerUuid, 'wats signer?')
-    if (text) {
-        client.publishCast(Auth.getUser.signerUuid, text)
-    } else toast("Cast needs to minimum 1 charachter")
+    client.publishCast(Auth.getUser.signerUuid, text)
 };
