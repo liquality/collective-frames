6e5d701c11efb977305f95c3ffc99b87cf33d9c3
Johanna Fransson <adrina0002@gmail.com>
2024-04-04 14:21:48 +0200

    fix possible undefined env vars

--- src/utils/nft.ts
+++ src/utils/nft.ts
@@ -6,51 +6,55 @@ import { mnemonicToAccount } from 'viem/accounts'
 import axios from "axios";
 
 
-export async function create1155Contract(creator: `0x${string}`, tokenMetaDataUri: string, name: string) {
-    const walletAccount = mnemonicToAccount(process.env.OPERATOR_MNEMONIC)
+export async function create1155Contract(tokenMetaDataUri: string, name: string) {
+    if (process.env.OPERATOR_MNEMONIC && process.env.OPERATOR_ADDRESS) {
+        const walletAccount = mnemonicToAccount(process.env.OPERATOR_MNEMONIC)
 
-    console.log(walletAccount, 'walletaccount????')
-    const walletClient = createWalletClient({
-        account: walletAccount,
-        chain: base,
-        transport: http("https://base-mainnet.g.alchemy.com/v2/47hMa2y_Ow1YLx3fAsb_VqRcbwrUd-Tx")
-    });
-    const publicClient = createPublicClient({
-        chain: base,
-        transport: http("https://base-mainnet.g.alchemy.com/v2/47hMa2y_Ow1YLx3fAsb_VqRcbwrUd-Tx")
-    });
-
-
-    if (publicClient) {
-        // @ts-ignore
-        const creatorClient = create1155CreatorClient({ publicClient });
-
-        //TODO: set the creator to the address that is connected with Farcaster instead of Liq OPERATOR
-        //Liq operator should only pay for gas
-        const { request } = await creatorClient.createNew1155Token({
-            contract: {
-                name,
-                uri: tokenMetaDataUri
-            },
-            tokenMetadataURI: tokenMetaDataUri,
-            account: creator,
-            mintToCreatorCount: 1,
-        });
-
-        const { request: simulateRequest } = await publicClient.simulateContract(request);
-        const { abi, address, functionName, args } = simulateRequest
-        const hash = await walletClient.writeContract({
+        console.log(walletAccount, 'walletaccount????')
+        const walletClient = createWalletClient({
             account: walletAccount,
-            abi,
-            address,
-            functionName,
-            args
+            chain: base,
+            transport: http("https://base-mainnet.g.alchemy.com/v2/47hMa2y_Ow1YLx3fAsb_VqRcbwrUd-Tx")
         });
-        console.log(hash, 'wat is hash?')
-        const receipt = await publicClient.waitForTransactionReceipt({ hash });
-        console.log(receipt, 'this should be nft reciept')
-        return receipt;
+        const publicClient = createPublicClient({
+            chain: base,
+            transport: http("https://base-mainnet.g.alchemy.com/v2/47hMa2y_Ow1YLx3fAsb_VqRcbwrUd-Tx")
+        });
+
+
+        if (publicClient) {
+            // @ts-ignore
+            const creatorClient = create1155CreatorClient({ publicClient });
+
+            //TODO: set the creator to the address that is connected with Farcaster instead of Liq OPERATOR
+            //Liq operator should only pay for gas
+            const { request } = await creatorClient.createNew1155Token({
+                contract: {
+                    name,
+                    uri: tokenMetaDataUri
+                },
+                tokenMetadataURI: tokenMetaDataUri,
+                account: process.env.OPERATOR_ADDRESS as `0x${string}`,
+                mintToCreatorCount: 1,
+            });
+
+            const { request: simulateRequest } = await publicClient.simulateContract(request);
+            const { abi, address, functionName, args } = simulateRequest
+            const hash = await walletClient.writeContract({
+                account: walletAccount,
+                abi,
+                address,
+                functionName,
+                args
+            });
+            console.log(hash, 'wat is hash?')
+            const receipt = await publicClient.waitForTransactionReceipt({ hash });
+            console.log(receipt, 'this should be nft reciept')
+            return receipt;
+        }
+
     }
+
 
 }
 
