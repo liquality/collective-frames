fc7fdae6c3cdcee4abdaff98c3d141392d74c9fd
Johanna Fransson <adrina0002@gmail.com>
2024-04-04 09:11:09 +0200

    add api service so we dont have to call axios routes in components

---
+++ src/utils/api-service.ts
@@ -0,0 +1,21 @@
+import { User } from "@neynar/nodejs-sdk/build/neynar-api/v2";
+import axios from "axios";
+
+const ApiService = {
+    authenticateUser: async function (neynarData: User) {
+        const { data } = await axios.post(`/api/user/`, neynarData);
+        return data
+    },
+    /* 
+        getUser: async function (userId: number) {
+            return null
+        },
+        getAllUsers: async function () {
+            return null
+        },
+    
+        updateUser: async function (userId: number, userObject: any) {
+            return null
+        } */
+}
+export default ApiService;
