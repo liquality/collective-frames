295aa61469102290f21d7786af49a5708910d3bf
Johanna Fransson <adrina0002@gmail.com>
2024-04-10 11:05:48 +0200

    try and fix live error with uploading nft

--- src/app/api/frames/route.ts
+++ src/app/api/frames/route.ts
@@ -11,68 +11,70 @@ export async function POST(request: NextRequest) {
   try {
 
     const form = await request.formData();
-    const user = await findUserByFid(form.get("createdBy") as unknown as number)
-    const data = {
-      name: (form.get("name") as string) || "",
-      description: (form.get("description") as string) || "",
-      imageFile: form.get("imageFile"),
-      collectiveId: (form.get("collectiveId") as string) || "",
-      createdBy: user?.id
-    };
-    console.log({ data })
-    const { name, description, collectiveId } = data;
-    // Generate unique id for frame to not use the integer 
-    const slug = uuidv4();
+    console.log()
+    const user = await findUserByFid(Number(form.get("createdBy")))
 
-    //1 upload the nft metadata to vercel
-    const ipfs = await pinFileToIPFS(data.imageFile, name, description);
+    if (user) {
 
-    if (ipfs) {
-      const tokenMetaData = {
-        name,
-        description,
-        image: ipfs.ipfsImageUrl,
-        imageURI: ipfs.ipfsImageUrl,
+      const data = {
+        name: (form.get("name") as string) || "",
+        description: (form.get("description") as string) || "",
+        imageFile: form.get("imageFile"),
+        collectiveId: (form.get("collectiveId") as string) || "",
+        createdBy: user?.id
       };
-      //2 upload the JSON metadatauri to vercel blob
-      const { url: metaDataUri } = await put(
-        "metadata_uri",
-        JSON.stringify(tokenMetaData),
-        { access: "public" }
-      );
-      //3) create the erc1155 using Liq Operator account as sponsor + Zora SDK
-      const nft = await create1155Contract(
-        metaDataUri,
-        name
-      );
+      console.log({ data })
+      const { name, description, collectiveId } = data;
+      // Generate unique id for frame to not use the integer 
+      const slug = uuidv4();
 
-      if (nft) {
-        //4) add new created nft mint frame to db so we can track
-        const newFrame = await db
-          .insert(frame)
-          .values({
-            name,
-            slug,
-            imageUrl: ipfs.ipfsGatewayUrl,
-            description,
-            collectiveId: Number(collectiveId),
-            metaDataUrl: metaDataUri,
-            tokenId: 1, //always 1 after creating new erc1155 contract with zora
-            tokenAddress: nft.logs[0].address,
-            createdBy: user?.id,
-          })
-          .returning();
-        return NextResponse.json({
-          frame: newFrame[0],
-          zoraUrl: `https://zora.co/collect/base:${nft.logs[0].address}/1`,
-        });
-      }
-      return NextResponse.json(
-        { error: "NFT failed to be created using Zora SDK" },
-        { status: 500 }
-      );
-    }
-    return NextResponse.json({ error: "Upload file failed" }, { status: 500 });
+      //1 upload the nft metadata to vercel
+      const ipfs = await pinFileToIPFS(data.imageFile, name, description);
+
+      if (ipfs) {
+        const tokenMetaData = {
+          name,
+          description,
+          image: ipfs.ipfsImageUrl,
+          imageURI: ipfs.ipfsImageUrl,
+        };
+        //2 upload the JSON metadatauri to vercel blob
+        const { url: metaDataUri } = await put(
+          "metadata_uri",
+          JSON.stringify(tokenMetaData),
+          { access: "public" }
+        );
+        //3) create the erc1155 using Liq Operator account as sponsor + Zora SDK
+        const nft = await create1155Contract(
+          metaDataUri,
+          name
+        );
+
+        if (nft) {
+          //4) add new created nft mint frame to db so we can track
+          const newFrame = await db
+            .insert(frame)
+            .values({
+              name,
+              slug,
+              imageUrl: ipfs.ipfsGatewayUrl,
+              description,
+              collectiveId: Number(collectiveId),
+              metaDataUrl: metaDataUri,
+              tokenId: 1, //always 1 after creating new erc1155 contract with zora
+              tokenAddress: nft.logs[0].address,
+              createdBy: user?.id,
+            })
+            .returning();
+          return NextResponse.json({
+            frame: newFrame[0],
+            zoraUrl: `https://zora.co/collect/base:${nft.logs[0].address}/1`,
+          });
+        } else { throw Error("NFT failed to be created using Zora SDK") }
+
+      } else { throw Error("Failed to upload NFT to IPFS") }
+
+    } else { throw Error("Failed to find user by fid: " + form.get("createdBy")) }
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error }, { status: 500 });
@@ -81,13 +83,13 @@ export async function POST(request: NextRequest) {
 
 export async function GET(request: NextRequest) {
   const fid = request.cookies.get(COOKIE_USER_FID)?.value;
-  if(fid) {
+  if (fid) {
     const users = await db.select().from(user).where(eq(user.fid, Number(fid))).limit(1);
-    if(users && users.length > 0) {
+    if (users && users.length > 0) {
 
-    const res = await db.select().from(frame).where(eq(frame.createdBy, users[0].id));
+      const res = await db.select().from(frame).where(eq(frame.createdBy, users[0].id));
 
-    return Response.json(res || []);
+      return Response.json(res || []);
     }
   }
   return Response.json({}, { status: 404 })
