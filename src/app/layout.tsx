6473071ac156ce640afa8feaa155b67e040ff2d6
Bradley Suira <bradleysuira@gmail.com>
2024-04-16 09:12:48 -0500

    fix: fixed follow option acording to Thessy request

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,16 +46,6 @@ export default function RootLayout({
             <div className="flex mt-8 justify-center items-center">
               {children}
             </div>
-            <div className="flex items-center justify-center mt-24">
-              <a
-                href="https://warpcast.com/liquality"
-                target="_blank"
-                style={{ minWidth: 320 }}
-                className="rounded-full text-center border border-white-500 px-4 py-2 bg-transparent disabled:opacity-75 text-white-500 focus:outline-none focus:ring-0"
-              >
-                Follow Liquality on Farcaster
-              </a>
-            </div>
             <OurVision />
           </main>
           <Footer></Footer>
